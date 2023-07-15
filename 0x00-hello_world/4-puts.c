629e29176eea1c56ee6f2c10d328470834da86bc
jamila <jamiii.el1994@gmail.com>
2023-07-15 09:42:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main -Entry point
+ *
+ * Description: print with puts fonction
+ *
+ * Return: Alawys 0 (success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
