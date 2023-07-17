3307014e9f450e1cc81866f8191426308ebe4926
jamila <jamiii.el1994@gmail.com>
2023-07-17 14:22:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all alphabet letters except a and e
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+int ch = 0;
+while (ch < 10)
+{
+printf("%i", ch);
+ch++;
+}
+printf("\n");
+return (0);
+}
