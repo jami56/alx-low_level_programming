1b21efdb7524aba5b6cf3de4e5d342fd12f63b40
jamila <jamiii.el1994@gmail.com>
2023-07-18 12:35:54 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int ch, i;
+for (i = 1; i <= 10; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+return (0);
+}
