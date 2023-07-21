2a6b89ca6981da143731d9f08f90e11c9a67df37
jamila <jamiii.el1994@gmail.com>
2023-07-21 04:16:01 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 void more_numbers(void)
 {
-    int i, j;
-
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j <= 14; j++)
-        {
-            if (j > 9) // For numbers greater than 9, print the tens digit
-                _putchar(j / 10 + '0');
-            _putchar(j % 10 + '0'); // Print the ones digit
-        }
-        _putchar('\n');
-    }
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10) 
+_putchar('1');
+_putchar(j % 10 + '0'); 
+}      
+putchar('\n');
+} 
 }
