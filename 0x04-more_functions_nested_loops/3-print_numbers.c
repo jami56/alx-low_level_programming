73dddfe1528d2cc9b3908d3d6fba7434ab7f19e0
jamila <jamiii.el1994@gmail.com>
2023-07-21 02:23:12 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
  * print_numbers - print 0 - 9
  *    only using _putchar twice
  * Return: Always 0 (Success)
-*/
+ */
 void print_numbers(void)
 {
 int sh = 0;
-do {
+while (sh <= 9)
+{
 _putchar(sh + '0');
 sh++;
 }
-while (sh >= 0 && sh <= 9)
 _putchar('\n');
 }
