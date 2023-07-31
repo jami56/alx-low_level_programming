be3c407c505b8ecc8ba722cf9e6bac646899d20d
jamila <jamiii.el1994@gmail.com>
2023-07-31 13:42:34 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,6 @@
  * @s: pointer to put the constant
  * @b: constant
  * @n: max bytes to use
- *
  * Return: pointer 
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -16,3 +15,4 @@ s[itr] = b;
 }
 return (s);
 }
+
